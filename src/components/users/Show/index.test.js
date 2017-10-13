3b0e789c6c68bddea0bd7df533340f989b8f623f
Leigh Silverstein <lsilversteinto@gmail.com>
2017-10-12 23:27:09 -0400

    tests passing

--- src/components/users/Show/index.test.js
+++ src/components/users/Show/index.test.js
@@ -1,9 +1,9 @@
 // @flow
 import React from 'react';
-import InternshipGrid from 'components/internships/Grid';
 import User from '@client/models/User';
 import { shallow } from 'enzyme';
 
+import InternshipsAndProjectsTabs from '../InternshipsAndProjectsTabs';
 import { ShowUser } from './';
 
 describe('ShowUser', ()=>{
@@ -15,7 +15,7 @@ describe('ShowUser', ()=>{
     };
     it('InternshipGrid', ()=>{
       const wrapper = setup();
-      expect(wrapper.find(InternshipGrid).length).toBe(1);
+      expect(wrapper.find(InternshipsAndProjectsTabs).length).toBe(1);
     });
   });
 });
