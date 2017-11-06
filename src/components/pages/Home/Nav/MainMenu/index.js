3a67a4b7d3589a41fb108c0095ece5b27973417a
Leigh Silverstein <lsilversteinto@gmail.com>
2017-11-06 09:45:21 -0500

    move mainmenu out of nav

---
+++ src/components/pages/Home/Nav/MainMenu/index.js
@@ -0,0 +1,91 @@
+// @flow
+import React, { PureComponent } from 'react';
+
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { flowRight } from 'lodash';
+
+import { MainNavDropdown, MainNavDropdownItem } from 'ui-kit';
+import userSelectors from '@client/selectors/users';
+import sessionSelectors from '@client/selectors/pages/sessions';
+
+import { openPrelogin, openPresignup } from '@client/actions/panels';
+
+import userActions from '@client/actions/users';
+import pageApplicationActions from '@client/actions/pages/applications';
+import searchActions from '@client/actions/pages/search';
+import sessionActions from '@client/actions/pages/session';
+
+export class MainMenu extends PureComponent {
+  props: Object;
+  render() {
+    const {
+      session,
+      onSignup,
+      onLogin,
+      user,
+      onLogout,
+      goToUser,
+      goToSearch,
+      goToApplications,
+    } = this.props;
+    const { token: loggedIn } = session;
+    const search = (
+      <MainNavDropdownItem onClick={goToSearch}>Search</MainNavDropdownItem>
+    );
+    return (
+      <MainNavDropdown icon="more_vert">
+        {!loggedIn && [
+          search,
+          <MainNavDropdownItem onClick={onSignup}>Signup</MainNavDropdownItem>,
+          <MainNavDropdownItem onClick={onLogin}>Login</MainNavDropdownItem>,
+        ]}
+        {loggedIn && [
+          <MainNavDropdownItem onClick={() => goToUser(user.id)}>
+            {user.username}
+          </MainNavDropdownItem>,
+          search,
+          <MainNavDropdownItem onClick={goToApplications}>
+            Applications
+          </MainNavDropdownItem>,
+          <MainNavDropdownItem onClick={onLogout}>Logout</MainNavDropdownItem>,
+        ]}
+      </MainNavDropdown>
+    );
+  }
+}
+
+const mapStateToProps = createStructuredSelector({
+  user: userSelectors.getUserFromSession(),
+  session: sessionSelectors.find(),
+});
+
+function mapDispatchToProps(dispatch: $$dispatch) {
+  return {
+    checkIfLoggedIn() {
+      dispatch(sessionActions.checkIfLoggedIn());
+    },
+    onSignup() {
+      dispatch(openPresignup);
+    },
+    onLogin() {
+      dispatch(openPrelogin);
+    },
+    onLogout() {
+      dispatch(sessionActions.logout());
+    },
+    goToUser(id) {
+      dispatch(userActions.goTo(id));
+    },
+    goToSearch() {
+      dispatch(searchActions.goTo());
+    },
+    goToApplications() {
+      dispatch(pageApplicationActions.goTo());
+    },
+  };
+}
+
+export default flowRight([connect(mapStateToProps, mapDispatchToProps)])(
+  MainMenu
+);
--- src/components/pages/Home/Nav/index.js
+++ src/components/pages/Home/Nav/index.js
@@ -3,94 +3,38 @@ import React, { PureComponent } from 'react';
 
 import { AppBar } from 'react-toolbox';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+
 import { flowRight } from 'lodash';
 
-import { MainNavDropdown, MainNavDropdownItem } from 'ui-kit';
-import userSelectors from '@client/selectors/users';
-import sessionSelectors from '@client/selectors/pages/sessions';
+import { Navigation } from 'ui-kit';
 
-import { openPrelogin, openPresignup } from '@client/actions/panels';
-
-import userActions from '@client/actions/users';
-import pageApplicationActions from '@client/actions/pages/applications';
 import searchActions from '@client/actions/pages/search';
-import sessionActions from '@client/actions/pages/session';
+
+import MainMenu from './MainMenu';
 
 export class Nav extends PureComponent {
   props: Object;
   render() {
-    const {
-      session,
-      onSignup,
-      onLogin,
-      user,
-      onLogout,
-      goToUser,
-      goToSearch,
-      goToApplications,
-    } = this.props;
-    const { token: loggedIn } = session;
-    const search = (
-      <MainNavDropdownItem onClick={goToSearch}>Search</MainNavDropdownItem>
-    );
     return (
-      <AppBar onLeftIconClick={goToSearch} title="Menternship" leftIcon="home">
-        <MainNavDropdown icon="more_vert">
-          {!loggedIn && [
-            search,
-            <MainNavDropdownItem onClick={onSignup}>
-              Signup
-            </MainNavDropdownItem>,
-            <MainNavDropdownItem onClick={onLogin}>Login</MainNavDropdownItem>,
-          ]}
-          {loggedIn && [
-            <MainNavDropdownItem onClick={() => goToUser(user.id)}>
-              {user.username}
-            </MainNavDropdownItem>,
-            search,
-            <MainNavDropdownItem onClick={goToApplications}>
-              Applications
-            </MainNavDropdownItem>,
-            <MainNavDropdownItem onClick={onLogout}>
-              Logout
-            </MainNavDropdownItem>,
-          ]}
-        </MainNavDropdown>
+      <AppBar
+        onLeftIconClick={this.props.goToSearch}
+        title="Menternship"
+        leftIcon="home"
+      >
+        <Navigation type="horizontal">
+          <MainMenu />
+        </Navigation>
       </AppBar>
     );
   }
 }
 
-const mapStateToProps = createStructuredSelector({
-  user: userSelectors.getUserFromSession(),
-  session: sessionSelectors.find(),
-});
-
 function mapDispatchToProps(dispatch: $$dispatch) {
   return {
-    checkIfLoggedIn() {
-      dispatch(sessionActions.checkIfLoggedIn());
-    },
-    onSignup() {
-      dispatch(openPresignup);
-    },
-    onLogin() {
-      dispatch(openPrelogin);
-    },
-    onLogout() {
-      dispatch(sessionActions.logout());
-    },
-    goToUser(id) {
-      dispatch(userActions.goTo(id));
-    },
     goToSearch() {
       dispatch(searchActions.goTo());
-    },
-    goToApplications() {
-      dispatch(pageApplicationActions.goTo());
     },
   };
 }
 
-export default flowRight([connect(mapStateToProps, mapDispatchToProps)])(Nav);
+export default flowRight([connect(null, mapDispatchToProps)])(Nav);
